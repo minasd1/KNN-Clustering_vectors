99fc0cda21709de4238cd8de2a52e7e51a27ce47
minasd1 <92537784+minasd1@users.noreply.github.com>
2021-10-26 17:47:24 +0300

    Upload corresponding header file

---
+++ file_functions.h
@@ -0,0 +1,16 @@
+#ifndef FILE_FUNCTIONS_H
+#define FILE_FUNCTIONS_H
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+void open_file(fstream* file, const char* filename, ios_base::openmode mode);
+void seek_in_file(fstream* file, streampos position);
+void close_file(fstream* file);
+
+
+
+#endif
