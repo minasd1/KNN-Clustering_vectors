f48b08335b9bab9ea704c020597ba55ee1be17c9
minasd1 <92537784+minasd1@users.noreply.github.com>
2021-10-27 15:26:58 +0300

    Deleted some unecessary comments on file_functions.cpp

--- file_functions.cpp
+++ file_functions.cpp
@@ -4,7 +4,7 @@ void open_file(fstream* file, const char* filename, ios_base::openmode mode){
 
     try{
         (*file).open(filename, mode);
-        ios_base::iostate exceptionMask = (*file).exceptions() | ios_base::failbit; //(*file).exceptions() | ios::failbit to allaksa se badbit kai douleve
+        ios_base::iostate exceptionMask = (*file).exceptions() | ios_base::failbit;
         (*file).exceptions(exceptionMask);
         
     }catch(ios_base::failure& e){
@@ -36,7 +36,7 @@ void close_file(fstream* file){
 
     try{
         (*file).close();
-        ios_base::iostate exceptionMask = (*file).exceptions() | ios_base::failbit; //(*file).exceptions() | 
+        ios_base::iostate exceptionMask = (*file).exceptions() | ios_base::failbit;
         (*file).exceptions(exceptionMask);
     }catch(ios_base::failure& e){
         cerr << e.what() << endl;
