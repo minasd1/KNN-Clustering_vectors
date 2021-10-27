c8681bf6af3496077d740de42b6c65f93644ee77
minasd1 <92537784+minasd1@users.noreply.github.com>
2021-10-28 00:48:26 +0300

    Added corresponding HashTable header file

---
+++ hashTable.h
@@ -0,0 +1,34 @@
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+static vector<vector <vector <int> > > HashTables;  //DECLARE A VECTOR OF HASHTABLES
+
+void hashTable_initialization(int num_of_hashTables, int num_of_buckets);
+void hashTable_print_data();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
