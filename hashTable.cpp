834bee0799fc7eaced54171abd52d62315c9a3e9
minasd <minas.dsp@gmail.com>
2021-10-29 19:39:39 +0300

    hashTable.cpp/h - Added a push_back and a get_points_in_buckets function

--- hashTable.cpp
+++ hashTable.cpp
@@ -3,8 +3,32 @@
 void hashTable_initialization(int num_of_hashTables, int num_of_buckets){
 
     //INITIALIZE L HASHTABLES WITH HASHTABLESIZE BUCKETS AND 1 POINT (WITH ID 0) IN EACH BUCKET
-    HashTables.resize(num_of_hashTables, vector<vector<int> >(num_of_buckets, vector<int>(1)));
+    HashTables.resize(num_of_hashTables, vector<vector<int> >(num_of_buckets, vector<int>(0)));
     //IT CAN ALSO BE INITIALIZED AS ZERO
+}
+
+//PUSH A POINT'S ID VALUE TO A BUCKET OF EACH OF THE HASHTABLES USING 
+//EACH BUCKET IS INDICATED BY THE INT VALUES PRODUCED BY FUNCTION G
+void hashTable_push_back(vector<int>& g,int key_val){
+
+    for(int i = 0; i < HashTables.size(); i++){
+        HashTables[i][g[i]].push_back(key_val);
+    }
+}
+
+//GET KEY VALUES OF ALL THE POINTS OF A BUCKET OF EACH HASH TABLE
+//EACH BUCKET IS INDICATED BY THE INT VALUES PRODUCED BY FUNCTION G 
+vector<int> hashTable_get_points_in_buckets(vector<int>& g){
+
+    vector<int> points_in_buckets;
+
+    for(int i = 0; i < HashTables.size(); i++){
+        for(int j = 0; j < HashTables[i][g[i]].size(); j++){
+            points_in_buckets.push_back(HashTables[i][g[i]][j]);
+        }
+    }
+
+    return points_in_buckets;
 }
 
 void hashTable_print_data(){
--- hashTable.h
+++ hashTable.h
@@ -10,6 +10,8 @@ using namespace std;
 static vector<vector <vector <int> > > HashTables;  //DECLARE A VECTOR OF HASHTABLES
 
 void hashTable_initialization(int num_of_hashTables, int num_of_buckets);
+void hashTable_push_back(vector<int>& g,int key_val);
+vector<int> hashTable_get_points_in_buckets(vector<int>& g);
 void hashTable_print_data();
 
 
